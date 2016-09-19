94608a2338baf7263fe2be1dc416faeccef94a23
richardson-souza <richardson.allan.souza@gmail.com>
2016-09-18 22:19:51 -0400

    update

---
+++ Factory/Pizza/ny_style_cheese_pizza.rb
@@ -0,0 +1,12 @@
+require_relative "pizza"
+
+class NYStyleCheesePizza < Pizza
+
+  def initialize
+    @name = "NY Style Sauce and Cheese Pizza"
+    @dough = "Thin Crust Dough"
+    @sauce = "Marinara Sauce"
+    @toppings << "Grated Reggiano Cheese"
+  end
+
+end
---
+++ Factory/Pizza/pizza.rb
@@ -0,0 +1,39 @@
+class Pizza
+  attr_accessor :name, :dough, :sauce, :toppings[]
+
+  def Prepare
+    puts "Prepare: #{@name}"
+    puts "Tossing dough..."
+    puts "Adding sauce..."
+    puts "Adding toppings: "
+    @toppings.each do |topping|
+      puts "    #{topping}"
+    end
+  end
+
+  def Bake
+    puts "Bake for 25 minutes at 350"
+  end
+
+  def Cut
+    puts "Cut the pizza into diagonal slices"
+  end
+
+  def Box
+    puts "Place pizza in official PizzaStore box"
+  end
+
+  def getName
+    return @name
+  end
+
+  def toString
+    show << "----  #{@name} ----"
+    show << "#{@dough}"
+    show << "#{@sauce}"
+    @toppings.each do |topping|
+      show << "#{topping}"
+    end
+  end
+
+end
